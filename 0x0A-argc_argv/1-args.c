96bc60944c1b27ed535caa8c0403850f5e7bc6b4
redd-stk <eddieochieng11@gmail.com>
2022-04-02 00:18:23 +0300

    new commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+/*
+ * 1-args.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments passed into thre function
+ * @argc: The argument count
+ * @argv: The argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
