4bc7f2bfa4ab5c1724a6fc26e55637d3c8627223
redd-stk <eddieochieng11@gmail.com>
2022-04-12 03:40:25 +0300

    new commit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+/*
+ * 5-free_dog.c
+ */
+
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs
+ * @d: pointer to dog
+ */
+
+void free_dog(dog_t *d)
+{
+	if (!d)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
